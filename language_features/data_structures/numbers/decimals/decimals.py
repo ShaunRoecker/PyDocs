8f424cc8bde9ebae5de8a54461db4d6321c04484
s-roecker <shaun.roecker@gmail.com>
2023-06-02 21:05:15 -0400

    decimals/floats

---
+++ language_features/data_structures/numbers/decimals/decimals.py
@@ -0,0 +1,122 @@
+import decimal
+from decimal import Decimal
+
+
+# decimals 
+# Decimals have a context that controls certain aspects of working with decimals
+    # precision during arithmetic operations
+    # rounding algorithm
+
+#  This context can be global => the default context
+#  or temporary(local) => sets temporary settings without affecting the global settings
+
+
+global_context = decimal.getcontext()
+print(global_context)
+# Context(
+#   prec=28, 
+#   rounding=ROUND_HALF_EVEN, 
+#   Emin=-999999, Emax=999999, 
+#   capitals=1, 
+#   clamp=0, 
+#   flags=[], 
+#   traps=[InvalidOperation, DivisionByZero, Overflow]
+# )
+
+
+local_context = decimal.localcontext(ctx=None)
+print(local_context)
+# returns a context manager(use a 'with' statement)
+
+
+# Precision and Rounding:
+ctx = decimal.getcontext() # context (global in this case)
+ctx.prec  # get or set the precision (value is an int)
+ctx.rounding # get or set the rounding mechanism(value is a string)
+
+
+# Global 
+# setting the context
+decimal.getcontext().rounding = decimal.ROUND_HALF_DOWN
+
+
+# Local
+# use as context manager
+with decimal.localcontext() as ctx:
+    ctx.prec = 2
+    ctx.rounding = decimal.ROUND_HALF_UP
+    # anything you do in here will use this context
+
+# it defaults back to global after you leave the context manager
+
+
+g_ctx = decimal.getcontext()
+
+print(type(g_ctx))  # <class 'decimal.Context'>
+
+print(type(decimal.localcontext())) # <class 'decimal.ContextManager'>
+
+x = Decimal('1.25')
+y = Decimal('1.35')
+
+with decimal.localcontext() as ctx:
+    ctx.prec = 6
+    ctx.rounding = decimal.ROUND_HALF_UP
+    # uses local context
+    print(round(x, 1))    # 1.3
+    print(round(y, 1))    # 1.4
+
+
+# uses global context
+print(round(x, 1))   # 1.2
+print(round(y, 1))   # 1.3
+
+
+# Constructors and Contexts
+integers = Decimal(10)
+strings = Decimal('0.1')
+tuples = Decimal((1, (3, 1, 4, 1, 5), -4)) # -3.1415 Decimal((sign, (d,i,g,i,t,s), exponent))
+
+
+print(tuples)
+
+
+to_decimal = lambda x: Decimal(str(x))
+
+
+decimal.getcontext().prec = 6
+
+a = Decimal('0.12345')
+b = Decimal('0.12345')
+print(a + b)
+
+
+# ****Dont create Decimals from floats*****
+# create a string before passing to Decimal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- language_features/data_structures/numbers/floats/float.py
+++ language_features/data_structures/numbers/floats/float.py
@@ -1,4 +1,5 @@
 from fractions import Fraction
+import math
 
 
 
@@ -12,8 +13,102 @@ print(a) # 0.1
 print(format(a, ".15f")) # 0.100000000000000
 print(format(a, ".25f")) # 0.1000000000000000055511151
 
+############################################################
+############################################################
+
 a = 0.1 + 0.1 + 0.1
 b = 0.3
 
 print(a == b) # False
+# To fix this... never use == on floats
+
+print(math.isclose(a, b)) # True
+
+#for large numbers, then look at relative tolerances
+#for small numbers, then look at absolute tolerances
+
+print(math.isclose(a, b, rel_tol=0.01, abs_tol=0.01))
+
+print(math.isclose(1.19, 1.19000001)) # False
+
+############################################################
+############################################################
+
+x = 10000.01
+y = 10000.02
+
+print(round(x, 3) == round(y, 3))
+
+print(y / x)
+
+print(format(x, '.25f')) # 0.1000000000000000055511151
+
+
+# 0.1 and 0.3 do not have finite binary representations,
+
+
+
+# Coercing Floats to Integer
+
+# Float --> Integer == Data loss
+
+# 1. Trucation
+print("Trucation")
+# Truncation simple return the integer part of the number
+print(math.trunc(10.4) == 10) # true
+print(int(10.4) == 10) # true
+
+
+# 2. Floor
+print("Floor")
+# The floor of a number is the largest integer less than 
+# or equal to the number
+print(math.floor(-10.4))  # -11
+print(math.floor(10.4)) # 10
+
+# Floor division
+print(10 // 3) # 3
+
+
+# 3. Ceiling
+print("Ceiling")
+# The ceiling of a number is the smallest integer greater than 
+# (or equal to) the number
+
+print(math.ceil(10.1)) # 11
+print(math.ceil(-10.1)) # -10
+
+
+#Rounding
+
+n = round(10.2, 0)
+print(n) # 10.0
+
+print(round(1.25, 1)) # 1.2
+
+
+# Banker's Rounding (least biases)
+print(round(15, -1))
+print(round(25, -1))
+
+# another implementation of bankers rounding (only for ints and not for negatives)
+bankers = lambda x: int(x + 0.5)
+
+print(bankers(10.3))
+print(bankers(10.9))
+
+
+
+print(round(1.8888, 3), round(1.8888, 2), round(1.8888, 1), round(1.8888, 0))
+
+
+def _round(x):
+    from math import copysign
+    return int(x + 0.5 * copysign(1, x))  # math.copysign(x, y) returns x with the sign (-/+) of y
+
+
+print(round(1.5), _round(1.5)) # 2  2
+print(round(2.5), _round(2.5)) # 2  3
+
+
 
